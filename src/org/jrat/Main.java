4b88d416756a3b9354b54f5f5592cb34874e4848
Kevin Schaap <kschaap1994@gmail.com>
2015-10-16 19:25:25 +0200

    Start of Project: jRAT

---
+++ src/org/jrat/Main.java
@@ -0,0 +1,11 @@
+package org.jrat;
+
+/**
+ * Created by Kevin on 16-10-2015.
+ */
+public class Main {
+
+    public static void main(final String... args) {
+        System.out.println("-----Initializing: jRat-----");
+    }
+}
